79bbc701a936accdbf40033f9178e118536f8c60
ned0emo <alexandr42suv@mail.ru>
2024-02-19 22:09:17 +0800

    Добавьте файлы проекта.

---
+++ BlocEncryptCmdServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using BlocEncryptCmdServer;
+
+var config = new ServerConfig();
+try
+{
+    await config.LoadConfig();
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Ошибка загрузки настроек:");
+    Console.WriteLine(ex.Message);
+}
+
+IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync("127.0.0.1");
+IPAddress ipAddress = ipHostInfo.AddressList.Where(address => !address.IsIPv6LinkLocal).First();
+IPEndPoint ipEndPoint = new(ipAddress, 11_000);
+
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+var handler = await listener.AcceptAsync();
+while (true)
+{
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    var eom = "<|EOM|>";
+    if (response.IndexOf(eom) > -1 /* is end of message */)
+    {
+        Console.WriteLine(
+            $"Socket server received message: \"{response.Replace(eom, "")}\"");
+
+        var ackMessage = "<|ACK|>";
+        var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+        await handler.SendAsync(echoBytes, 0);
+        Console.WriteLine(
+            $"Socket server sent acknowledgment: \"{ackMessage}\"");
+
+        break;
+    }
+    // Sample output:
+    //    Socket server received message: "Hi friends 👋!"
+    //    Socket server sent acknowledgment: "<|ACK|>"
+}
---
+++ BlocEncryptCmdServer/ServerConfig.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlocEncryptCmdServer
+{
+    public enum ConfigKey
+    {
+        ChatSecret,
+        PasswordSecret,
+    }
+
+    public class ServerConfig
+    {
+        //public const string userNameCfg = "User";
+        //public const string chatSecretCfg = "ChatSecret";
+        //public const string passwordSecretCfg = "PasswordSecret";
+
+        const string cfgFileName = "./config.txt";
+        const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        readonly Dictionary<string, string> configs;
+
+        public ServerConfig()
+        {
+            configs = [];
+        }
+
+        public async Task LoadConfig()
+        {
+            configs.Clear();
+
+            if (!File.Exists(cfgFileName))
+            {
+                StreamWriter sw = new(cfgFileName);
+                await sw.WriteAsync("");
+                sw.Close();
+            }
+
+            StreamReader sr = new(cfgFileName);
+            var content = await sr.ReadToEndAsync();
+            sr.Close();
+
+            var configStrings = content
+                        .Replace("\r", "")
+                        .Split("\n")
+                        .ToList();
+            configStrings.RemoveAll(conf => !conf.Contains('='));
+
+            foreach (var confStr in configStrings)
+            {
+                var pair = confStr.Split('=');
+                configs.Add(pair[0].Trim(), pair[1].Trim());
+            }
+
+            if (!configs.ContainsKey(ConfigKey.ChatSecret.ToString()))
+            {
+                Console.WriteLine("Введите кодовое слово для шифрования сообщений (кириллица, только заглавные буквы):");
+                string? secret;
+                do
+                {
+                    secret = Console.ReadLine();
+
+                    if (secret != null)
+                    {
+                        var flag = false;
+                        foreach (var ch in secret)
+                        {
+                            if (!alphabet.Contains(ch))
+                            {
+                                flag = true;
+                                break;
+                            }
+                        }
+
+                        if (flag) continue;
+                        break;
+                    }
+                } while (true);
+
+                configs.Add(ConfigKey.ChatSecret.ToString(), secret);
+            }
+
+            if (!configs.ContainsKey(ConfigKey.PasswordSecret.ToString()))
+            {
+                Console.WriteLine("Введите кодовое слово для шифрования пароля (кириллица, только заглавные буквы):");
+                string? secret;
+                do
+                {
+                    secret = Console.ReadLine();
+
+                    if (secret != null)
+                    {
+                        var flag = false;
+                        foreach (var ch in secret)
+                        {
+                            if (!alphabet.Contains(ch))
+                            {
+                                flag = true;
+                                break;
+                            }
+                        }
+
+                        if (flag) continue;
+                        break;
+                    }
+                } while (true);
+
+                configs.Add(ConfigKey.PasswordSecret.ToString(), secret);
+            }
+
+            await SaveConfig();
+        }
+
+        public async Task SaveConfig()
+        {
+            if (configs.Count == 0) return;
+
+            var output = configs.Select(cfg => cfg.Key + '=' + cfg.Value).Aggregate((a, b) => a + '\n' + b);
+
+            StreamWriter sw = new(cfgFileName);
+            await sw.WriteAsync(output);
+            sw.Close();
+        }
+
+        public async Task SetValue(ConfigKey key, string value)
+        {
+            configs.Add(key.ToString(), value);
+            await SaveConfig();
+        }
+
+        public string? GetValue(ConfigKey key)
+        {
+            if (configs.ContainsKey(key.ToString()))
+            {
+                return configs[key.ToString()];
+            }
+
+            return null;
+        }
+    }
+}
